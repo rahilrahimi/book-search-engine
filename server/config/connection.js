bd028bcd326cc1947100cbd9fd7c688f33171ad8
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-02-01 17:32:07 -0500

    initial change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// HELLO 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
